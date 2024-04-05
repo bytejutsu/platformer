c8220e81a0825679ccb0a18ba56de40fad707572
Dhia Nabli <bytejutsuproject@gmail.com>
2024-04-05 14:43:55 +0100

    added a level as a background

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -6,10 +6,38 @@ class Game extends Phaser.Scene {
     super({ key: 'GameScene' });
   }
 
-  // init(data) {}
-
   preload() {
+
+    this.load.tilemapTiledJSON('level-1', 'assets/tilemaps/level-1.json');
+
+    this.load.image('world-1-sheet', 'assets/tilesets/world-1.png');
+
+    this.load.spritesheet('hero-idle-sheet', 'assets/hero/idle.png', {
+      frameWidth: 32,
+      frameHeight: 64,
+    });
+
     this.load.spritesheet('hero-run-sheet', 'assets/hero/run.png', {
+      frameWidth: 32,
+      frameHeight: 64,
+    });
+
+    this.load.spritesheet('hero-pivot-sheet', 'assets/hero/pivot.png', {
+      frameWidth: 32,
+      frameHeight: 64,
+    });
+
+    this.load.spritesheet('hero-jump-sheet', 'assets/hero/jump.png', {
+      frameWidth: 32,
+      frameHeight: 64,
+    });
+
+    this.load.spritesheet('hero-flip-sheet', 'assets/hero/spinjump.png', {
+      frameWidth: 32,
+      frameHeight: 64,
+    });
+
+    this.load.spritesheet('hero-fall-sheet', 'assets/hero/fall.png', {
       frameWidth: 32,
       frameHeight: 64,
     });
@@ -19,13 +47,9 @@ class Game extends Phaser.Scene {
 
     this.cursorKeys = this.input.keyboard.createCursorKeys();
 
-    this.input.keyboard.on('keydown-SPACE', () => {
-      console.log('pressed space');
-    });
-
-    this.space = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-    this.space.on('up', () => {
-      console.log('Release Space');
+    this.anims.create({
+      key: 'hero-idle',
+      frames: this.anims.generateFrameNumbers('hero-idle-sheet'),
     });
 
     this.anims.create({
@@ -35,16 +59,46 @@ class Game extends Phaser.Scene {
       repeat: -1,
     });
 
+    this.anims.create({
+      key: 'hero-pivoting',
+      frames: this.anims.generateFrameNumbers('hero-pivot-sheet'),
+    });
+
+    this.anims.create({
+      key: 'hero-jumping',
+      frames: this.anims.generateFrameNumbers('hero-jump-sheet'),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: 'hero-flipping',
+      frames: this.anims.generateFrameNumbers('hero-flip-sheet'),
+      frameRate: 30,
+      repeat: 0,
+    });
+
+    this.anims.create({
+      key: 'hero-falling',
+      frames: this.anims.generateFrameNumbers('hero-fall-sheet'),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.addMap();
+
     this.hero = new Hero(this, 250, 160);
-  }
-
-  update(time, delta) {
-
-    if(this.cursorKeys.space.isDown){
-      console.log('Holding Space');
-    }
 
   }
+
+  addMap() {
+    this.map = this.make.tilemap({ key:'level-1' });
+    const groundTiles = this.map.addTilesetImage('world-1', 'world-1-sheet');
+
+    this.map.createStaticLayer('Ground', groundTiles);
+  }
+
+  update(time, delta) {}
 }
 
 export default Game;
