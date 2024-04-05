c98ab93dd66830381e1891238081c9d6df2c6323
Dhia Nabli <bytejutsuproject@gmail.com>
2024-04-05 21:07:24 +0100

    finished designing a test level

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,5 +1,6 @@
 import Phaser from 'phaser';
 import Hero from '../entities/Hero';
+import {assertObject} from "@babel/core/lib/config/validation/option-assertions";
 
 class Game extends Phaser.Scene {
   constructor() {
@@ -10,7 +11,13 @@ class Game extends Phaser.Scene {
 
     this.load.tilemapTiledJSON('level-1', 'assets/tilemaps/level-1.json');
 
-    this.load.image('world-1-sheet', 'assets/tilesets/world-1.png');
+    this.load.spritesheet('world-1-sheet', 'assets/tilesets/world-1.png', {
+      frameWidth: 32,
+      frameHeight: 32,
+      margin: 1,
+      spacing: 2,
+    });
+    this.load.image('clouds-sheet', 'assets/tilesets/clouds.png');
 
     this.load.spritesheet('hero-idle-sheet', 'assets/hero/idle.png', {
       frameWidth: 32,
@@ -38,6 +45,11 @@ class Game extends Phaser.Scene {
     });
 
     this.load.spritesheet('hero-fall-sheet', 'assets/hero/fall.png', {
+      frameWidth: 32,
+      frameHeight: 64,
+    });
+
+    this.load.spritesheet('hero-die-sheet', 'assets/hero/bonk.png', {
       frameWidth: 32,
       frameHeight: 64,
     });
@@ -85,41 +97,82 @@ class Game extends Phaser.Scene {
       repeat: -1,
     });
 
+    this.anims.create({
+      key: 'hero-dead',
+      frames: this.anims.generateFrameNumbers('hero-die-sheet'),
+    });
+
     this.addMap();
     this.addHero();
 
     this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
-    this.cameras.main.startFollow(this.hero);
   }
 
   addHero() {
-    this.hero = new Hero(this, 250, 160);
+    this.hero = new Hero(this, this.spawnPos.x, this.spawnPos.y);
+
+    this.cameras.main.startFollow(this.hero);
 
     this.children.moveTo(this.hero, this.children.getIndex(this.map.getLayer('Foreground').tilemapLayer));
 
-    this.physics.add.collider(this.hero, this.map.getLayer('Ground').tilemapLayer);
+    const groundCollider = this.physics.add.collider(this.hero, this.map.getLayer('Ground').tilemapLayer);
+
+    const spikesCollider = this.physics.add.overlap(this.hero, this.spikeGroup, () => {
+      this.hero.kill();
+    });
+
+    this.hero.on('died', () => {
+      groundCollider.destroy();
+      spikesCollider.destroy();
+      this.hero.body.setCollideWorldBounds(false);
+      this.cameras.main.stopFollow();
+    });
+
   }
 
   addMap() {
     this.map = this.make.tilemap({ key:'level-1' });
     const groundTiles = this.map.addTilesetImage('world-1', 'world-1-sheet');
+    const backgroundTiles = this.map.addTilesetImage('clouds', 'clouds-sheet');
+
+    const backgroundLayer = this.map.createStaticLayer('Background', backgroundTiles);
+    backgroundLayer.setScrollFactor(0.6);
 
     const groundLayer = this.map.createStaticLayer('Ground', groundTiles);
     groundLayer.setCollision([1, 2, 4], true);
 
 
-    this.map.createStaticLayer('Foreground', groundTiles);
-
-
     this.physics.world.setBounds(0,0, this.map.widthInPixels, this.map.heightInPixels);
     this.physics.world.setBoundsCollision(true, true, false, true);
 
+    this.spikeGroup = this.physics.add.group({ immovable: true, allowGravity: false});
+
+    this.map.getObjectLayer('Objects').objects.forEach(object => {
+      if(object.name == 'Start'){
+        this.spawnPos = { x: object.x, y: object.y }
+      }
+      if(object.gid === 7){
+        const spike = this.spikeGroup.create(object.x, object.y, 'world-1-sheet', object.gid - 1);
+        spike.setOrigin(0,1);
+        spike.setSize(object.width - 10, object.height - 10);
+        spike.setOffset(5, 10);
+      }
+    });
+
+    this.map.createStaticLayer('Foreground', groundTiles);
 
     //const debugGraphics = this.add.graphics();
     //groundLayer.renderDebug(debugGraphics);
   }
 
-  update(time, delta) {}
+  update(time, delta) {
+    const cameraBottom = this.cameras.main.getWorldPoint(0, this.cameras.main.height).y;
+
+    if(this.hero.isDead() && this.hero.getBounds().top > cameraBottom + 100){
+      this.hero.destroy();
+      this.addHero();
+    }
+  }
 }
 
 export default Game;
